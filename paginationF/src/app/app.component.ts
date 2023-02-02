480fe77334c62372e594e848d800b09144685d20
panvla <pandvlado@gmail.com>
2023-02-02 17:53:31 +0100

    pagination done.

--- paginationF/src/app/app.component.ts
+++ paginationF/src/app/app.component.ts
@@ -14,6 +14,9 @@ export class AppComponent implements OnInit {
   
   usersState$: Observable<{appState: string, appData?: ApiResponse<Page>, error?: HttpErrorResponse}>;
   responseSubject = new BehaviorSubject<ApiResponse<Page>>(null);
+  private currentPageSubject=  new BehaviorSubject<number>(0);
+  currentPage$ = this.currentPageSubject.asObservable();
+
 
   constructor(private userService: UserService){}
 
@@ -21,6 +24,7 @@ export class AppComponent implements OnInit {
     this.usersState$ = this.userService.users$().pipe(
       map((response: ApiResponse<Page>)=>{
         this.responseSubject.next(response);
+        this.currentPageSubject.next(response.data.page.number);
         console.log(response);
         return ({appState:'APP_LOADED', appData:response});
       }
@@ -34,12 +38,17 @@ export class AppComponent implements OnInit {
     this.usersState$ = this.userService.users$(name, pageNumber).pipe(
       map((response: ApiResponse<Page>)=>{
         this.responseSubject.next(response);
+        this.currentPageSubject.next(pageNumber);
         console.log(response);
         return({appState: 'APP_LOADED', appData: response});
       }),
       startWith({appState: 'APP_LOADED', appData: this.responseSubject.value}),
       catchError((error: HttpErrorResponse)=> of({appState: 'APP_ERROR', error}))
     );
+  }
+
+  goToNextOrPreviousPage(direction?: string, name?:string): void {
+    this.goToPage(name, direction === 'forward' ? this.currentPageSubject.value + 1 : this.currentPageSubject.value - 1);
   }
 
 }
